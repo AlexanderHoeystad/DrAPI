d1f24be360849a2f7775d8aaa1f5dac78ea5a7ec
Nuggitt <Phulip.nielsen@gmail.com>
2024-04-24 10:03:22 +0200

    push modelclass

--- DrAPI/MusicRecord.cs
+++ DrAPI/MusicRecord.cs
@@ -15,27 +15,61 @@
         }
 
 
-        public void Validate()
+        public void ValidateTitle()
         {
             if (string.IsNullOrEmpty(Title))
             {
                 throw new Exception("Title is required");
             }
+            if (Title.Length < 2)
+            {
+                throw new Exception("Title must be at least 2 characters long");
+            }
+        }
+
+        public void ValidateArtist()
+        {
             if (string.IsNullOrEmpty(Artist))
             {
                 throw new Exception("Artist is required");
             }
+            if (Artist.Length < 2)
+            {
+                throw new Exception("Artist must be at least 2 characters long");
+            }
+        }
+
+        public void ValidateDuration()
+        {
             if (Duration == null)
             {
                 throw new Exception("Duration is required");
             }
-            if (publicationYear == null)
+            if (Duration < 0)
             {
-                throw new Exception("publicationYear is required");
+                throw new Exception("Duration must be a positive number");
             }
         }
 
+        public void ValidatePublicationYear()
+        {
+            if (publicationYear == null)
+            {
+                throw new Exception("Publication year is required");
+            }
+            if (publicationYear < 1860)
+            {
+                throw new Exception("Publication year must be higher than 1860");
+            }
+        }
 
+        public void Validate()
+        {
+            ValidateTitle();
+            ValidateArtist();
+            ValidateDuration();
+            ValidatePublicationYear();
+        }
 
     }
 
