c79efe69d86813e87b0ef49b392db20ab17aba9e
AlexanderHoeystad <Alek369@hotmail.com>
2024-04-24 09:51:41 +0200

    Validate

--- DrAPI/MusicRecord.cs
+++ DrAPI/MusicRecord.cs
@@ -15,8 +15,30 @@
         }
 
 
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Title))
+            {
+                throw new Exception("Title is required");
+            }
+            if (string.IsNullOrEmpty(Artist))
+            {
+                throw new Exception("Artist is required");
+            }
+            if (Duration == null)
+            {
+                throw new Exception("Duration is required");
+            }
+            if (publicationYear == null)
+            {
+                throw new Exception("publicationYear is required");
+            }
+        }
+
+
 
     }
+
 
 
 
