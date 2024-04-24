7a52c8120e2a531657b5acf13102593223825f43
AlexanderHoeystad <Alek369@hotmail.com>
2024-04-24 09:47:18 +0200

    Add project files.

---
+++ DrAPI/MusicRecord.cs
@@ -0,0 +1,23 @@
+﻿namespace DrAPI
+{
+    public class MusicRecord
+    {
+        public string? Title { get; set; }
+        public string? Artist { get; set; }
+        public int? Duration { get; set; }
+        public int? publicationYear { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"{Title} by {Artist} ({publicationYear}) {Duration}";
+        }
+
+
+
+    }
+
+
+
+}
